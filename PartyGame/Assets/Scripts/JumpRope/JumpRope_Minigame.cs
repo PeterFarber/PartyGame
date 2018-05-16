7954c5617163e00c90e8ccdd19b3e115bd61089d
Richmond Brininger <richmondbrininger@gmail.com>
2018-05-16 16:08:07 -0400

    Finished base JumpRopeGame

---
+++ PartyGame/Assets/Scripts/JumpRope/JumpRope_Minigame.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpRoap_Minigame : MonoBehaviour {
+
+    public GameObject jumpRope;
+    public GameObject bar;
+
+    public GameObject[] players;
+    public GameObject[] barriers;
+
+    int playerCount = 0;
+
+    public AudioClip win;
+    private AudioSource audioSource;
+
+    private GameObject[] hexagon;
+    private GameObject selectedHexagon;
+
+    private float countdown = 3.0f;
+    private int direction = 1;
+
+    private float yPos;
+    private float startPos;
+
+    private float resetTime;
+    private bool moving = false;
+    private bool running = false;
+
+    private string startButton = "P1_START";
+
+
+    // Use this for initialization
+    void Start () {
+        //Set the reset timer to the countDown time.
+        resetTime = countdown;
+        barriers[0] = bar;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("P1_JUMP"))
+        {
+            if (CheckFirstPlayer() == true)
+            {
+                startButton = "P1_START";
+            }
+            players[0].SetActive(true);
+
+        }
+
+        if (Input.GetButtonDown("P2_JUMP"))
+        {
+            if (CheckFirstPlayer() == true)
+            {
+                startButton = "P2_START";
+            }
+            players[1].SetActive(true);
+        }
+
+        if (Input.GetButtonDown("P3_JUMP"))
+        {
+            if (CheckFirstPlayer() == true)
+            {
+                startButton = "P3_START";
+            }
+            players[2].SetActive(true);
+        }
+
+        if (Input.GetButtonDown("P4_JUMP"))
+        {
+            if (CheckFirstPlayer() == true)
+            {
+                startButton = "P4_START";
+            }
+            players[3].SetActive(true);
+        }
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            UpdateRope();
+        }
+    }
+
+    int PlayerCount()
+    {
+        int sum = 0;
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].activeInHierarchy == true)
+            {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    private void UpdateRope()
+    {
+        jumpRope.transform.Rotate(0,5,0);
+        foreach (GameObject item in barriers)
+        {
+            if (item != selectedHexagon)
+            {
+                item.transform.position = new Vector3(item.transform.position.x, startPos, item.transform.position.z);
+            }
+        }
+    }
+
+    bool CheckFirstPlayer()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].activeSelf == true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ PartyGame/Assets/jumpMotion.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jumpMotion : MonoBehaviour
+{
+
+    public GameObject[] Players;
+
+    public GameObject SoundPlayer;
+
+    public AudioClip WinningClip;
+
+
+    private AudioSource _audioSource;
+
+    public  GameObject _jumpRope;
+
+    public GameObject[] _platforms;
+
+    public float _waveTimer;
+    public bool _nextWave;
+    public float _shakeTimer;
+    public bool _shaking;
+    private float _startTimer;
+    public bool _atStart;
+
+    private bool _running;
+    private string _startButton;
+    private int _playerCount;
+
+    private float _rotateSpeed;
+    private float _count;
+    private bool _speedingUp;
+    private bool _transition;
+
+    private Joystick_Controller _joystickController;
+
+    private Score_Controller _scoreController;
+
+    void Start()
+    {
+
+        _joystickController = GameObject.Find("Global_Controller").GetComponent<Joystick_Controller>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (_joystickController.CheckPlayer(i))
+            {
+                Players[i].GetComponent<Player_Controller>().setJoystick(_joystickController.PlayerJoystick(i));
+                Players[i].SetActive(true);
+            }
+        }
+
+        _scoreController = GameObject.Find("Global_Controller").GetComponent<Score_Controller>();
+
+        //Initialize Varaibles!
+        _rotateSpeed = 4f;
+        _startTimer = 0f;
+        _waveTimer = 2f;
+        _running = false;
+        _startButton = "";
+        _playerCount = 0;
+        _atStart = true;
+        _speedingUp = true;
+        _shakeTimer = Random.Range(1.0f, 100.0f);
+        _count = 0;
+        _audioSource = GetComponent<AudioSource>();
+
+        _running = true;
+        _playerCount = PlayerCount();
+
+        for (int i = 0; i < _playerCount; i++)
+        {
+            _platforms[i].SetActive(true);
+        }
+    }
+
+    IEnumerator EndGameCheck()
+    {
+        if (_running)
+        {
+            _playerCount = PlayerCount();
+            if (_playerCount == 1)
+            {
+                _audioSource.clip = WinningClip;
+                _audioSource.Play();
+                _running = false;
+                yield return new WaitForSeconds(_audioSource.clip.length);
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    void Update()
+    {
+        //for (int i = 0; i < _playerCount; i++)
+        //{
+        //    _platforms[i].SetActive(true);
+        //}
+        if (_speedingUp == true)
+        {
+            _rotateSpeed += 0.01f;
+        }
+        else
+        {
+            _rotateSpeed -= 0.01f;
+        }
+        if(_rotateSpeed > 5)
+        {
+
+            _speedingUp = false;
+        }
+        if(_rotateSpeed < 2){
+            _speedingUp = true;
+        }
+        if (_startTimer > 50)
+        {
+            _jumpRope.transform.Rotate(_rotateSpeed, 0, 0);
+        }
+        else
+        {
+            _startTimer += 0.1f;
+        }
+        StartCoroutine(EndGameCheck());
+
+
+    }
+
+    int PlayerCount()
+    {
+        int sum = 0;
+        for (int i = 0; i < Players.Length; i++)
+        {
+            if (Players[i].activeInHierarchy == true)
+            {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+}
